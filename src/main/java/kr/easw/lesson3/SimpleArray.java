53ecfe39bfb2413bc7b24ce5ea18e99aa15a653d
yuvnn <119295341+yuvnn@users.noreply.github.com>
2024-03-28 13:19:51 +0900

    과제 제출

--- src/main/java/kr/easw/lesson3/SimpleArray.java
+++ src/main/java/kr/easw/lesson3/SimpleArray.java
@@ -1,7 +1,5 @@
 package kr.easw.lesson3;
 
-import java.util.Arrays;
-
 public class SimpleArray {
     private static int[] arrays = new int[10];
 
@@ -34,6 +32,7 @@ public class SimpleArray {
      * * ^2는 제곱의 의미로 사용되었습니다.
      */
     private static void fillArray(int index) {
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+        arrays[index] = index*7/5;
+        arrays[index] *= arrays[index];
     }
 }
